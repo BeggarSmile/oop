bf82c78ac4f27930b5e32fa41fa17ba88491a2a7
BeggarSmile <bihappy13@yandex.ru>
2019-11-17 20:55:02 +0300

    Second Lab

--- src/RPIS71/Kazachenko/wdad/learn/xml/TestXmlTask.java
+++ src/RPIS71/Kazachenko/wdad/learn/xml/TestXmlTask.java
@@ -2,10 +2,9 @@ package RPIS71.Kazachenko.wdad.learn.xml;
 
 public class TestXmlTask {
 
-    public static void main(String[] args) throws Exception{
-        XmlTask task = new XmlTask("src/RPIS71/Kazachenko/wdad/learn/xml/organizm.xml");
-        System.out.println(task.salaryAverage());
-        System.out.println(task.salaryAverage("HR"));
-        task.setSalary("Sokolov", "Slava", 40000);
+    public static void main(String[] args) {
+        XmlTask task = new XmlTask("src/RPIS71/Kazachenko/wdad/learn/xml/poopnfork.xml");
+        task.changeOfficiantName("fedor", "petrov", "patra", "kukrova");
     }
 }
+
--- src/RPIS71/Kazachenko/wdad/learn/xml/XmlTask.java
+++ src/RPIS71/Kazachenko/wdad/learn/xml/XmlTask.java
@@ -15,117 +15,98 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
-import java.rmi.NoSuchObjectException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XmlTask {
 
     private final String filePath;
+    private final Document doc;
 
-    XmlTask(String filePath) {
+    public XmlTask(String filePath) {
         this.filePath = filePath;
+        this.doc = parseDocument();
     }
 
-    int salaryAverage() {
-        int summary = 0;
-        int count = 0;
-        Document doc = parseDocument();
-        assert doc != null;
-        NodeList salaryes = doc.getElementsByTagName("salary");
-        Element salaryTag = null;
-
-        for (int i = 0; i < salaryes.getLength(); i++) {
-            salaryTag = (Element) salaryes.item(i);
-            summary += Integer.parseInt(salaryTag.getTextContent());
-            count++;
-        }
-
-        return (summary / count);
-
-    }
-
-    int salaryAverage(String departmentName) throws Exception{
-        int summary = 0;
-        int count = 0;
-        int corr = -1;
-        Document doc = parseDocument();
-        assert doc != null;
-        NodeList departments = doc.getElementsByTagName("department");
-        Element departmentTag = null;
-
-        for (int i = 0; i < departments.getLength(); i++) {
-            departmentTag = (Element) departments.item(i);
-            if (departmentName.equals(departmentTag.getAttribute("name"))) {
-                corr = i;
-                break;
-            }
-        }
-        if (corr == -1) throw new Exception("Заданный департамент отсутсвует");
-
-        NodeList salaryes = departmentTag.getElementsByTagName("salary");
-        Element salaryTag = null;
-        for (int i = 0; i < salaryes.getLength(); i++) {
-            salaryTag = (Element) salaryes.item(i);
-            summary += Integer.parseInt(salaryTag.getTextContent());
-            count++;
-        }
-
-        return (summary / count);
-    }
-
-    void setSalary(String firstName, String secondName, int newSalary) throws Exception{
-        int corr = -1;
-        Document doc = parseDocument();
-        assert doc != null;
-        NodeList employes = doc.getElementsByTagName("employee");
-        Element employeeTag = null;
-        String thisFirstname, thisSecondName;
-
-        for (int i = 0; i < employes.getLength(); i++) {
-            employeeTag = (Element) employes.item(i);
-            thisFirstname = employeeTag.getAttribute("firstname");
-            thisSecondName = employeeTag.getAttribute("secondname");
-            if ((firstName.equals(thisFirstname))
-                    && secondName.equals(thisSecondName)) {
-                corr = i;
-                break;
+    public int earningsTotal(String officiantSecondName, LocalDate date) throws NoSuchDayException {
+        Element dateElement = getDate(date);
+        NodeList orders = dateElement.getElementsByTagName("order");
+        Element order, currentTotalCost, officiant;
+        String thisSecondName;
+        double totalCost = 0;
+        for (int i = 0; i < orders.getLength(); i++) {
+            order = (Element) orders.item(i);
+            officiant = (Element) order.getElementsByTagName("officiant").item(0);
+            if (officiant != null) {
+                thisSecondName = officiant.getAttribute("secondname");
+                if (officiantSecondName.equalsIgnoreCase(thisSecondName)) {
+                    currentTotalCost = (Element) order.getLastChild();
+                    if (currentTotalCost != null) {
+                        totalCost += Double.parseDouble((currentTotalCost.getTextContent()));
+                    } else {
+                        totalCost += countTotalCost(order);
+                    }
+                }
             }
         }
 
-        if (corr == -1)
-            throw new Exception("Человек не найден");
+        return (int) totalCost;
+    }
 
-        NodeList salaryes = doc.getElementsByTagName("salary");
-        Element salaryTag = null;
+    public void removeDay(LocalDate date) throws NoSuchDayException {
+        Element dateElement = getDate(date);
+        dateElement.getParentNode().removeChild(dateElement);
+        changeDocument(doc);
+    }
 
-        for (int i = 0; i < salaryes.getLength(); i++) {
-            salaryTag = (Element) salaryes.item(i);
-            if (i == corr) salaryTag.setTextContent(String.valueOf(newSalary));
+    public void changeOfficiantName(String oldFirstName, String oldSecondName, String newFirstName, String newSecondName) {
+        NodeList officiants = doc.getElementsByTagName("officiant");
+        Element officiantElement;
+        for (int i = 0; i < officiants.getLength(); i++) {
+            officiantElement = (Element) officiants.item(i);
+            if (isThisOfficiant(officiantElement, oldFirstName, oldSecondName)) {
+                officiantElement.setAttribute("firstname", newFirstName.toLowerCase());
+                officiantElement.setAttribute("secondname", newSecondName.toLowerCase());
+            }
         }
         changeDocument(doc);
     }
 
-    void fireEmployee(String firstName, String secondName) {
-        Document doc = parseDocument();
-        assert doc != null;
-        NodeList employees = doc.getElementsByTagName("employee");
-        Element employeeTag = null;
-        String thisFirstName, thisSecondName;
-
-        for (int i = 0; i < employees.getLength(); i++) {
-            employeeTag = (Element) employees.item(i);
-            thisFirstName = employeeTag.getAttribute("firstname");
-            thisSecondName = employeeTag.getAttribute("secondname");
-            if (thisFirstName.equals(employeeTag.getAttribute("firstname"))
-                    && thisSecondName.equals(employeeTag.getAttribute("secondName")))
-                employeeTag.getParentNode().removeChild(employeeTag);
+    public List<Order> getOrders(LocalDate date) throws NoSuchDayException {
+        List<Order> orders = new ArrayList<>();
+        Element dateElement = getDate(date);
+        List<Item> items;
+        Element currentElement;
+        Officiant officiant;
+        NodeList orderElements = dateElement.getChildNodes();
+        for (int i = 0; i < orderElements.getLength(); i++) {
+            currentElement = (Element) orderElements.item(i);
+            officiant = getOfficiant(currentElement);
+            items = getItems(currentElement);
+            orders.add(new Order(officiant, items));
         }
-        changeDocument(doc);
+        return orders;
+    }
+
+    public LocalDate getLastOfficiantWorkDate(String firstName, String secondName) throws NoSuchOfficiantException {
+        NodeList officiants = doc.getElementsByTagName("officiant");
+        Element officiantElement;
+        Element dateElement;
+        for (int i = officiants.getLength() - 1; i >= 0; i--) {
+            officiantElement = (Element) officiants.item(i);
+            if (isThisOfficiant(officiantElement, firstName, secondName)) {
+                dateElement = (Element) officiantElement.getParentNode().getParentNode();
+                return parseDate(dateElement);
+            }
+        }
+        throw new NoSuchOfficiantException("Оффициант с таким именем и фамилией отсутствует");
     }
 
     private Document parseDocument() {
         try {
             DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+            domFactory.setIgnoringElementContentWhitespace(true);
             domFactory.setValidating(true);
             return domFactory.newDocumentBuilder().parse(new InputSource(filePath));
         } catch (SAXException | IOException | ParserConfigurationException e) {
@@ -137,13 +118,86 @@ public class XmlTask {
     private void changeDocument(Document doc) {
         try {
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
             transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "restaurant.dtd");
             transformer.transform(new DOMSource(doc), new StreamResult(filePath));
             System.out.println("XML file updated successfully");
         } catch (TransformerException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private Element getDate(LocalDate searchingDate) throws NoSuchDayException {
+        NodeList dates = doc.getElementsByTagName("date");
+        Element dateElement;
+        LocalDate thisDate;
+        for (int i = 0; i < dates.getLength(); i++) {
+            dateElement = (Element) dates.item(i);
+            thisDate = parseDate(dateElement);
+            if (searchingDate.isEqual(thisDate)) {
+                return dateElement;
+            }
+        }
+        throw new NoSuchDayException("Заданный день отсутствует.");
+    }
+
+    private Officiant getOfficiant(Element element) {
+        Officiant officiant = new Officiant();
+        Element officiantAttributes = (Element) element.getElementsByTagName("officiant").item(0);
+        if (officiantAttributes != null) {
+            officiant.setFirstName(officiantAttributes.getAttribute("firstname"));
+            officiant.setSecondName(officiantAttributes.getAttribute("secondname"));
+        }
+        return officiant;
+    }
+
+    private List<Item> getItems(Element element) {
+        List<Item> items = new ArrayList<>();
+        NodeList itemsElements = element.getElementsByTagName("item");
+        Element itemElement;
+        for (int i = 0; i < itemsElements.getLength(); i++) {
+            itemElement = (Element) itemsElements.item(i);
+            if (itemElement != null) {
+                items.add(new Item(itemElement.getAttribute("name"),
+                        Integer.parseInt("0" + itemElement.getAttribute("cost"))));
+
+            }
+        }
+        return items;
+    }
+
+    private boolean isThisOfficiant(Element officiant, String firstName, String secondName) {
+        return firstName.equalsIgnoreCase(officiant.getAttribute("firstname")) &&
+                secondName.equalsIgnoreCase(officiant.getAttribute("secondname"));
+    }
+
+    private LocalDate parseDate(Element dateTag) {
+        return LocalDate.parse(String.format("%s-%s-%s",
+                dateTag.getAttribute("year"),
+                dateTag.getAttribute("month"),
+                dateTag.getAttribute("day")));
+    }
+
+    private void addTotalCostTag(Element order, int totalCost) {
+        Element totalCostElement = doc.createElement("totalcost");
+        totalCostElement.appendChild(doc.createTextNode(String.valueOf(totalCost)));
+        order.appendChild(totalCostElement);
+        changeDocument(doc);
+    }
+
+    private int countTotalCost(Element order) {
+        NodeList items = order.getElementsByTagName("item");
+        int newTotalCost = 0;
+        Element item;
+        for (int j = 0; j < items.getLength(); j++) {
+            item = (Element) items.item(j);
+            newTotalCost += Integer.parseInt(item.getAttribute("cost"));
+        }
+        addTotalCostTag(order, newTotalCost);
+        return newTotalCost;
     }
 
 }
